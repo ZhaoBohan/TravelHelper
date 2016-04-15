f4bf1108f75c74d53c64af540b7e30af0b77977d
ZhaoBohan <zhaobohan27@gmail.com>
2016-04-15 18:01:29 +0800

    Connect two activities and transport city name

--- app/src/main/java/cn/edu/tongji/travelhelper/MainActivity.java
+++ app/src/main/java/cn/edu/tongji/travelhelper/MainActivity.java
@@ -1,7 +1,12 @@
 package cn.edu.tongji.travelhelper;
 
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.startSearch);
+        button.setOnClickListener(btnOnClick);
     }
+
+    private Button.OnClickListener btnOnClick = new Button.OnClickListener(){
+        public void onClick(View v){
+            String city;
+            EditText edittext = (EditText) findViewById(R.id.cityName);
+            city = edittext.getText().toString();
+            if (city.equals("")){
+                return;
+            }
+            Intent intent = new Intent();
+            intent.setClass(MainActivity.this, ShowActivity.class);
+            intent.putExtra("City", city);
+            MainActivity.this.startActivity(intent);
+        }
+    };
 }
--- app/src/main/java/cn/edu/tongji/travelhelper/ShowActivity.java
+++ app/src/main/java/cn/edu/tongji/travelhelper/ShowActivity.java
@@ -1,7 +1,9 @@
 package cn.edu.tongji.travelhelper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ShowActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class ShowActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
+
+        Intent intent = getIntent();
+        String TextValue = intent.getStringExtra("City");
+        TextView text = (TextView) findViewById(R.id.textView);
+        text.setText(TextValue);
     }
 }
