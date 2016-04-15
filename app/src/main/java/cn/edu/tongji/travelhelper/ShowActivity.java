2e38a76d6b205fd400ac36ee98b0d953a1bcaf2d
ZhaoBohan <zhaobohan27@gmail.com>
2016-04-16 00:39:24 +0800

    Add tabs

--- app/src/main/java/cn/edu/tongji/travelhelper/ShowActivity.java
+++ app/src/main/java/cn/edu/tongji/travelhelper/ShowActivity.java
@@ -3,6 +3,7 @@ package cn.edu.tongji.travelhelper;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 import android.widget.TextView;
 
 public class ShowActivity extends AppCompatActivity {
@@ -12,9 +13,16 @@ public class ShowActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
 
-        Intent intent = getIntent();
+        /*Intent intent = getIntent();
         String TextValue = intent.getStringExtra("City");
         TextView text = (TextView) findViewById(R.id.textView);
-        text.setText(TextValue);
+        text.setText(TextValue);*/
+
+        TabHost tabhost = (TabHost) findViewById(R.id.tabHost);
+        tabhost.setup();
+        tabhost.addTab(tabhost.newTabSpec("one").setIndicator("Weather").setContent(R.id.linearLayout));
+        tabhost.addTab(tabhost.newTabSpec("two").setIndicator("Bus").setContent(R.id.linearLayout2));
+        tabhost.addTab(tabhost.newTabSpec("three").setIndicator("Transport").setContent(R.id.linearLayout3));
+        tabhost.addTab(tabhost.newTabSpec("four").setIndicator("Map").setContent(R.id.linearLayout4));
     }
 }
